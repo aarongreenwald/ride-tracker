73cb13901f60f45bb8b15180a29ecfe1cd87bfd0
Aaron Greenwald <aarong@wix.com>
2019-02-16 10:19:26 +0200

    improved styling/design/ux on trip list

--- app/src/main/java/com/greenwald/aaron/ridetracker/TripRecyclerViewAdapter.kt
+++ app/src/main/java/com/greenwald/aaron/ridetracker/TripRecyclerViewAdapter.kt
@@ -1,10 +1,11 @@
 package com.greenwald.aaron.ridetracker
 
-import android.graphics.Color
 import android.support.v7.widget.RecyclerView
+import android.view.HapticFeedbackConstants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 
 import com.greenwald.aaron.ridetracker.TripListFragment.OnListFragmentInteractionListener
@@ -21,21 +22,27 @@ class TripRecyclerViewAdapter internal constructor(private val trips: List<Trip>
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.trip = trips[position]
 
-        if (LocationTrackingService.recordingTripId == holder.trip!!.id) {
-            holder.nameTextView.setTextColor(Color.GREEN)
-            holder.distanceTextView.setTextColor(Color.GREEN)
-        }
+        holder.tripRunningIndicator.visibility =
+                if (LocationTrackingService.recordingTripId == holder.trip!!.id)
+                    View.VISIBLE
+                else
+                    View.GONE
 
 
         holder.nameTextView.text = trips[position].name
         holder.distanceTextView.text = trips[position].distance.toString()
+        holder.tripTimeTextView.text = trips[position].elapsedTime.toString()
+
 
         holder.view.setOnClickListener {
+            holder.view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
             interactionListener?.onTripPress(holder.trip!!)
         }
 
         holder.view.setOnLongClickListener {
+            holder.view.performHapticFeedback(HapticFeedbackConstants.LONG_PRESS)
             interactionListener!!.onTripLongPress(holder.trip!!)
+
         }
     }
 
@@ -44,6 +51,8 @@ class TripRecyclerViewAdapter internal constructor(private val trips: List<Trip>
     inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
         val nameTextView: TextView = view.findViewById(R.id.tripName)
         val distanceTextView: TextView = view.findViewById(R.id.tripDistance)
+        val tripTimeTextView = view.findViewById<TextView>(R.id.tripTime)
+        val tripRunningIndicator = view.findViewById<ProgressBar>(R.id.tripRunningIndicator)
         var trip: Trip? = null
 
         override fun toString(): String {
